8e6d653cff12c554d320808a604d09bd8f96e9dc
Zac Smith <zrs3141592@gmail.com>
2019-07-29 21:15:12 -0600

    make logo link to dashboard

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,10 +5,12 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <header className="navbar">
-      <img
-        src="https://fontmeme.com/permalink/190724/8f2bd2e8d36ee8f404723b6c674e437a.png"
-        alt="Disney Parent logo"
-      />
+      <Link to="/dashboard">
+        <img
+          src="https://fontmeme.com/permalink/190724/8f2bd2e8d36ee8f404723b6c674e437a.png"
+          alt="Disney Parent logo"
+        />
+      </Link>
       <ul className="nav-links">
         <Link className="link" to="/signin">
           <li>Sign In / Sign Up</li>
