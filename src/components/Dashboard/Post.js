b72d9676c94156ee2e2893145caea1f419774df2
Zac Smith <zrs3141592@gmail.com>
2019-07-30 00:35:52 -0600

    add font-awesome and style post controls

--- src/components/Dashboard/Post.js
+++ src/components/Dashboard/Post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { genericTypeAnnotation } from "@babel/types";
 
 export default function Post(props) {
   return (
@@ -7,7 +8,32 @@ export default function Post(props) {
       <p>Where: {props.post.attraction}</p>
       <p>Chilren: {props.post.children} </p>
       <p>When: {props.post.time}</p>
-      <p className="post-control">reply | edit | delete</p>
+      <p className="post-control">
+        <span className="post-control-span">
+          <i class="fas fa-reply" style={replyStyle} />
+          reply
+        </span>
+        <span className="post-control-span">
+          <i class="fas fa-edit" style={editStyle} />
+          edit
+        </span>
+        <span className="post-control-span">
+          <i class="fas fa-trash" style={deleteStyle} />
+          delete
+        </span>
+      </p>
     </article>
   );
 }
+
+const replyStyle = {
+  color: "green"
+};
+
+const editStyle = {
+  color: "blue"
+};
+
+const deleteStyle = {
+  color: "red"
+};
